7af86afaa44823cf7baadab242306335432cf10f
Vadim Brik <77822375+vadimbrik-dev@users.noreply.github.com>
2022-01-17 17:17:38 +0600

    Update bookvar_base.dart

--- lib/src/bookvar_base.dart
+++ lib/src/bookvar_base.dart
@@ -6,26 +6,30 @@ import 'package:html/parser.dart' as html;
 typedef Chapter = List<Element>;
 typedef Book = List<Chapter>;
 
-abstract class Element {}
+abstract class Element {
+  final int reference;
+
+  Element({required this.reference});
+}
 
 abstract class TextElement extends Element {
   final String content;
 
-  TextElement({required this.content}) : super();
+  TextElement({required this.content, required int reference}) : super(reference: reference);
 }
 
 abstract class BlockElement extends Element {
   final double aspectRatio;
 
-  BlockElement({required this.aspectRatio});
+  BlockElement({required this.aspectRatio, required int reference}) : super(reference: reference);
 }
 
 class Header extends TextElement {
-  Header(String content) : super(content: content);
+  Header(String content, {required int reference}) : super(content: content, reference: reference);
 }
 
 class Paragraph extends TextElement {
-  Paragraph(String content) : super(content: content);
+  Paragraph(String content, {required int reference}) : super(content: content, reference: reference);
 }
 
 abstract class Image extends BlockElement {
@@ -34,23 +38,24 @@ abstract class Image extends BlockElement {
   Image({
     required this.buffer,
     required double aspectRatio,
-  }) : super(aspectRatio: aspectRatio);
+    required int reference
+  }) : super(aspectRatio: aspectRatio, reference: reference);
 
-  factory Image.from(List<int> buffer) {
+  factory Image.from(List<int> buffer, {required int reference}) {
     if (PngImage._hasPngSignature(buffer)) {
-      return PngImage(buffer);
+      return PngImage(buffer, reference: reference);
     }
-    return UniversalImage(buffer);
+    return UniversalImage(buffer, reference: reference);
   }
 }
 
 class UniversalImage extends Image {
-  UniversalImage(List<int> buffer) : super(buffer: buffer, aspectRatio: 1);
+  UniversalImage(List<int> buffer, {required int reference}) : super(buffer: buffer, aspectRatio: 1, reference: reference);
 }
 
 class PngImage extends Image {
-  PngImage(List<int> buffer)
-      : super(buffer: buffer, aspectRatio: _calculateAspectRatio(buffer));
+  PngImage(List<int> buffer, {required int reference})
+      : super(buffer: buffer, aspectRatio: _calculateAspectRatio(buffer), reference: reference);
 
   static const int widthBytesOffset = 16;
   static const int heightBytesOffset = 20;
@@ -82,6 +87,8 @@ List<Element> parse(String content, List<ImageRecord> images) {
   final nodes = document.querySelectorAll(selector);
   final elements = <Element>[];
 
+  var index = 0;
+
   for (final node in nodes) {
     if (_isImage(node)) {
       final source = node.attributes['src'];
@@ -93,15 +100,15 @@ List<Element> parse(String content, List<ImageRecord> images) {
       final record =
       images.firstWhere((element) => source.contains(element.filename));
 
-      elements.add(Image.from(record.buffer));
+      elements.add(Image.from(record.buffer, reference: index++));
     }
 
     if (_isEmpty(node)) {
       continue;
     } else if (_isHeader(node)) {
-      elements.add(Header(node.text));
+      elements.add(Header(node.text, reference: index++));
     } else if (_isParagraph(node)) {
-      elements.add(Paragraph(node.text));
+      elements.add(Paragraph(node.text, reference: index++));
     }
   }
 
